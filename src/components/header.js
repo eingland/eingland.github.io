4786afbebca9bb481925a9ee699c8607348c417a
Eric Ingland <ingland.eric@gmail.com>
2019-10-11 11:31:05 -0500

    Comment out other pages for now

--- src/components/header.js
+++ src/components/header.js
@@ -10,7 +10,8 @@ const Header = ({ siteTitle }) => (
       <Navbar.Collapse id="basic-Navbar-nav">
         <Nav className="mr-auto">
           <Nav.Link href="/blog">Blog</Nav.Link>
-          <Nav.Link href="/tags">Tags</Nav.Link>
+          {/*<Nav.Link href="/tags">Tags</Nav.Link>
+          <Nav.Link href="/projects">Projects</Nav.Link>*/}
         </Nav>
       </Navbar.Collapse>
     </Navbar>
