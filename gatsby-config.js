3ab79c6c2189306861650a0cdf25e48bab90472d
Eric Ingland <ingland.eric@gmail.com>
2019-10-16 19:28:02 -0500

    Add Favicon, fix feed generator warning.

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,7 +26,59 @@ module.exports = {
       }
     },
     'gatsby-plugin-sitemap',
-    'gatsby-plugin-feed',
+    {
+      resolve: 'gatsby-plugin-feed',
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allMarkdownRemark } }) => {
+              return allMarkdownRemark.edges.map(edge => {
+                return Object.assign({}, edge.node.frontmatter, {
+                  description: edge.node.excerpt,
+                  date: edge.node.frontmatter.date,
+                  url: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  custom_elements: [{ 'content:encoded': edge.node.html }]
+                })
+              })
+            },
+            query: `
+              {
+                allMarkdownRemark(
+                  sort: { order: DESC, fields: [frontmatter___date] },
+                ) {
+                  edges {
+                    node {
+                      excerpt
+                      html
+                      fields { slug }
+                      frontmatter {
+                        title
+                        date
+                      }
+                    }
+                  }
+                }
+              }
+            `,
+            output: '/rss.xml',
+            title: "Eric Ingland's RSS Feed"
+          }
+        ]
+      }
+    },
     'gatsby-plugin-netlify',
     'gatsby-plugin-react-helmet',
     {
@@ -138,10 +190,10 @@ module.exports = {
         name: 'gatsby-ericinglandcom',
         short_name: 'ericinglandcom',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
-        display: 'minimal-ui'
-        // icon: ``, // This path is relative to the root of the site.
+        background_color: '#000000',
+        theme_color: '#000000',
+        display: 'minimal-ui',
+        icon: './src/images/favicon.png' // This path is relative to the root of the site.
       }
     },
     'gatsby-plugin-offline',
